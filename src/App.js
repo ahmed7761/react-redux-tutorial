93665403bd75c2126df5394b1c7d8762bc794e55
Sharif Ahmed <sharif@namespaceit.com>
2021-03-16 11:15:19 +0600

    setState, prevState, props in setState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hello from "./components/Hello";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
         <Hello name="Sharif"/>
         <Hello>Child element</Hello>
+        <Counter/>
     </div>
   );
 }
