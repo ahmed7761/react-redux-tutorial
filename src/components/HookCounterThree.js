8816226be2b38d496965bc621d964e74340e07f8
Sharif Ahmed <sharif@namespaceit.com>
2021-03-22 12:25:14 +0600

    useEffect with condition

--- src/components/HookCounterThree.js
+++ src/components/HookCounterThree.js
@@ -3,13 +3,17 @@ import React, { useState, useEffect } from 'react';
 const HookCounterThree = () => {
 
     const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
 
     useEffect(() => {
+        console.log('use effect')
         document.title = `count: ${count}`
-    })
+    }, [count])
 
     return (
         <div>
+            <input type="text" value={name} onChange={e => setName(e.target.value)}/>
+            <h2>Name is {name}</h2>
             <h1>Value: {count}</h1>
             <button onClick={() => setCount(count+1)}>Increment</button>
         </div>
